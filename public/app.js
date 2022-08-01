446b043b669341ad0c7d149d44227b3b03aaa3ca
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-08-01 16:50:26 +0200

    add modules and interfaces

--- public/app.js
+++ public/app.js
@@ -1,4 +1,30 @@
-"use strict";
+//  interfaces
+const me = {
+    name: 'Alex',
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent, amount');
+        return amount;
+    },
+};
+const greetPerson = (person) => {
+    console.log(person.age);
+};
+greetPerson(me);
+import Invoice from './classes/invoice.js';
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+console.log(invOne, invTwo);
+let invoices = [];
+invoices.push(invOne);
+console.log(invoices);
+invoices.push(invTwo);
+invoices.forEach((inv) => {
+    console.log(inv.client, inv.details, inv.amount, inv.format());
+});
 const from = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
 const text = document.querySelector('#text');
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,52 @@
+//  interfaces
 
-const from = document.querySelector('.new-item-form')as HTMLFormElement;
-const type = document.querySelector('#type') as HTMLSelectElement;
-const text = document.querySelector('#text') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+interface IsPerson {
+  name: string
+  age: number
+  speak(a: string): void
+  spend(a: number): number
+}
 
-from.addEventListener('submit', (e:Event)=>{
-    e.preventDefault();
-    console.log(
-        type.value,
-        text.value,
-        details.value,
-        amount.valueAsNumber
-    )
+const me: IsPerson = {
+  name: 'Alex',
+  age: 30,
+  speak(text: string): void {
+    console.log(text)
+  },
+  spend(amount: number): number {
+    console.log('I spent, amount')
+    return amount
+  },
+}
+
+const greetPerson = (person: IsPerson) => {
+  console.log(person.age)
+}
+greetPerson(me)
+
+import Invoice from './classes/invoice.js'
+
+const invOne = new Invoice('mario', 'work on the mario website', 250)
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300)
+
+console.log(invOne, invTwo)
+
+let invoices: Invoice[] = []
+
+invoices.push(invOne)
+console.log(invoices)
+invoices.push(invTwo)
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.details, inv.amount, inv.format())
 })
 
+const from = document.querySelector('.new-item-form') as HTMLFormElement
+const type = document.querySelector('#type') as HTMLSelectElement
+const text = document.querySelector('#text') as HTMLInputElement
+const details = document.querySelector('#details') as HTMLInputElement
+const amount = document.querySelector('#amount') as HTMLInputElement
+
+from.addEventListener('submit', (e: Event) => {
+  e.preventDefault()
+  console.log(type.value, text.value, details.value, amount.valueAsNumber)
+})
